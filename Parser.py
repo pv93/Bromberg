f9d43f07601eba2522d9e2fe1e4322ce9eb6c5dd
pv93 <pvaysberg@gmail.com>
2014-05-26 00:54:22 -0400

    Update command line options, can now take directories of .ptt files

--- Parser.py
+++ Parser.py
@@ -1,9 +1,9 @@
 import argparse
-import os.path
+import os, os.path
 import dictbuild
 
 parser = argparse.ArgumentParser("Parser used to scan .ptt and .out files")
-parser.add_argument('-p', '--ptt', help = 'The ptt file(s) to be used', nargs = '*')
+parser.add_argument('-p', '--ptt', help = 'The ptt file(s) to be used, can also input a directory', nargs = '*')
 parser.add_argument('-o', '--out', help = 'The .out file to be used')
 parser.add_argument('-d', '--dest', help = 'Destination of output files')
 parser.add_argument('-i', '--iter', type = int, default= 1, help ="Iteration of psi-blast to use")
@@ -18,6 +18,7 @@ if not outfile:
     outfile = raw_input('Enter the .out file to be used: ')
 
 pttfiles = args.ptt
+
 if not pttfiles:
     while True:
         x = raw_input("You have not entered any .ptt files! Enter here, one by one. Press 'q' to stop: ")
@@ -28,7 +29,13 @@ if not pttfiles:
         else:
             pttfiles.append(x)
 
-iteration = args.iter
+for index, dircheck in enumerate(pttfiles): # checking if the user input a directory for the -p option, note that they could use a combination
+    if os.path.isdir(dircheck):
+        for files in os.listdir(dircheck):
+            if files[-4:] == '.ptt':
+                abspath = os.path.abspath(dircheck + "//" + files)
+                pttfiles.append(abspath)            
+        del pttfiles[index]
 
 # Building our lovely dictionaries
 dicts = dictbuild.dictbuild()
