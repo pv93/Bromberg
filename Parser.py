5568ed1539155ba81d0c5baa927a9d823f0133e6
pv93 <pvaysberg@gmail.com>
2014-07-02 21:20:44 -0400

    Updated parser

--- Parser.py
+++ Parser.py
@@ -14,10 +14,14 @@ parser.add_argument('-i', '--iter', type = int, default= 1, help ="Iteration of 
 args = parser.parse_args()
 if args.dest:
     save_path = args.dest
+else:
+    save_path = ""
 
 outfile = args.out
 if not outfile:
     outfile = raw_input('Enter the .out file to be used: ')
+
+outfile = os.path.join(save_path, outfile)
 
 pickledcogdict = args.pcd
 
@@ -27,6 +31,7 @@ if not pickledcogdict:
 # Opening the PID : COG dictionary
 with open(pickledcogdict, 'rb') as handle: 
     cogdict = marshal.load(handle)
+
 
 fp2count = 0 # FALSE POSITIVE 2 | BLAST result with : no query PID/no self hit, hits subject, that has a COG
 fp2file = open(outfile + " false positives 2.txt", 'w') # These results will be output into a file. Note that we are assuming that a gene with no PID is the same thing as a gene with a PID that doesn't hit itself
@@ -48,6 +53,14 @@ fncount = 0 # FALSE NEGATIVE | query has PID, query has COG, hits nothing
 
 uneg = 0 # UNCERTAIN NEGATIVES | query has PID, query has no COG, hits nothing
 
+def write(query, hit, filetype):
+    if filetype == "testset":
+        if os.stat(testset).st_size == 0:
+            testset.write("# This is the test set. Here you can find the queries that have PID's and COG's, that hit proteins without COG's. \n # In addition, queries with PIDS but no COG family that hit subjects (regardless of whether the subject has a COG family or not) also go into this file.")
+            testset.write("Query\tSubject\t
+        
+        
+    
 
 def self_hit_check(pid, hits):
     for hit in hits:
@@ -61,15 +74,60 @@ def self_hit_check(pid, hits):
 
 def evaluate_hits(query, list_of_hits, dictionary):
 
+    if len(list_of_hits) == 0:
+        nohits = True
+
     pids = query[2:]
     if len(pids) == 0:
         nopid = True
 
-    else:
+    if nopid and nohits:    # TRUE NEGATIVE 2
+        global tn2count += 1
+        return
 
-        for pid in pids:
-            self_hit = self_hit_check(pid, list_of_hits)
+    elif nopid and not nohits: # TRUE NEGATIVE 1 & FALSE POSITIVE 2
+        for hit in hits:
+            get_hit = hit.split('\t')[1]
+            hit_pid = get_hit.split('|')[1]
+
+            if hit_pid in cogdict:
+                global fp2count += 1
+            else:
+                global tn1count += 1
+
             
+            
+
+    track_self_hits = [] # let's try this
+    for pid in pids:
+        self_hit = self_hit_check(pid, list_of_hits)
+
+        if not self_hit and nohits:    # TRUE NEGATIVE 2 ... copy and pasting. should find a way to avoid doing this
+            global tn2count += 1
+            return
+    
+        elif not self_hit and not nohits: # TRUE NEGATIVE 1 & FALSE POSITIVE 2
+            for hit in hits:
+                get_hit = hit.split('\t')[1]
+                hit_pid = get_hit.split('|')[1]
+
+                if hit_pid in cogdict:  # TN1
+                    global fp2count += 1
+                else:                   #FP2
+                    global tn1count += 1
+
+        ## Now we have covered the cases where there is no PID or no self-hit. 
+
+        else:
+            if pid in cogdict:
+
+            else:
+               if nohits:
+                   uneg += 1
+                else:
+                    write
+            
+                
         
     
     
